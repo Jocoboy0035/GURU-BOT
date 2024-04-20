0638487e23cc495d98bc8f8aa7ebdd90cd6aac94
Jocoboy0035 <167625450+Jocoboy0035@users.noreply.github.com>
2024-04-20 09:50:21 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['+255753061468', 'Jbot_joco', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
